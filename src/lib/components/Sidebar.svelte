c4892207230c366e5c925c6f4816f9b6cdc53c7e
harungokcegoz <harungokcegoz92@gmail.com>
2025-01-15 16:25:26 +0100

    sidebar is added

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="h-[calc(100vh-4rem)] bg-gray-800 flex flex-col overflow-y-auto">
+	<nav class="flex-1 px-2 py-4 space-y-1">
+		<a
+			href="/"
+			class="flex items-center px-2 py-2 text-sm font-medium rounded-md"
+			class:text-white={$page.url.pathname === '/'}
+			class:bg-gray-900={$page.url.pathname === '/'}
+			class:text-gray-300={$page.url.pathname !== '/'}
+			class:hover:bg-gray-700={$page.url.pathname !== '/'}
+			class:hover:text-white={$page.url.pathname !== '/'}
+		>
+			<ion-icon 
+				name="home-outline" 
+				class="mr-3 h-6 w-6"
+				aria-hidden="true"
+			></ion-icon>
+			Home
+		</a>
+
+		<a
+			href="/todos"
+			class="flex items-center px-2 py-2 text-sm font-medium rounded-md"
+			class:text-white={$page.url.pathname === '/todos'}
+			class:bg-gray-900={$page.url.pathname === '/todos'}
+			class:text-gray-300={$page.url.pathname !== '/todos'}
+			class:hover:bg-gray-700={$page.url.pathname !== '/todos'}
+			class:hover:text-white={$page.url.pathname !== '/todos'}
+		>
+			<ion-icon 
+				name="list-outline" 
+				class="mr-3 h-6 w-6"
+				aria-hidden="true"
+			></ion-icon>
+			Review Todos
+		</a>
+
+		<a
+			href="/todos/create"
+			class="flex items-center px-2 py-2 text-sm font-medium rounded-md"
+			class:text-white={$page.url.pathname === '/todos/create'}
+			class:bg-gray-900={$page.url.pathname === '/todos/create'}
+			class:text-gray-300={$page.url.pathname !== '/todos/create'}
+			class:hover:bg-gray-700={$page.url.pathname !== '/todos/create'}
+			class:hover:text-white={$page.url.pathname !== '/todos/create'}
+		>
+			<ion-icon 
+				name="add-outline" 
+				class="mr-3 h-6 w-6"
+				aria-hidden="true"
+			></ion-icon>
+			Create Todo
+		</a>
+
+		<a
+			href="/todos/completed"
+			class="flex items-center px-2 py-2 text-sm font-medium rounded-md"
+			class:text-white={$page.url.pathname === '/todos/completed'}
+			class:bg-gray-900={$page.url.pathname === '/todos/completed'}
+			class:text-gray-300={$page.url.pathname !== '/todos/completed'}
+			class:hover:bg-gray-700={$page.url.pathname !== '/todos/completed'}
+			class:hover:text-white={$page.url.pathname !== '/todos/completed'}
+		>
+			<ion-icon 
+				name="checkmark-done-outline" 
+				class="mr-3 h-6 w-6"
+				aria-hidden="true"
+			></ion-icon>
+			Completed Todos
+		</a>
+	</nav>
+</div> 
